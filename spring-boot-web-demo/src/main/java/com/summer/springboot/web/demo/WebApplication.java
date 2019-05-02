08be761be03d4a31530202ec4a88839a9bb76c69
xia45399 <453993887@qq.com>
2019-05-02 11:07:50 +0800

    重命名demo项目

--- spring-boot-web-demo/src/main/java/com/summer/springboot/web/demo/WebApplication.java
+++ spring-boot-web-demo/src/main/java/com/summer/springboot/web/demo/WebApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MybatisApplication {
+public class WebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MybatisApplication.class, args);
+        SpringApplication.run(WebApplication.class, args);
     }
 }
