d8c07e308cf66dca4f62e4cd3cf80d1dcf730e55
xia45399 <453993887@qq.com>
2019-05-04 21:32:21 +0800

    定义全局的服务主机地址 配置redis

--- spring-boot-web-demo/src/main/java/com/summer/springboot/web/demo/pojo/Student.java
+++ spring-boot-web-demo/src/main/java/com/summer/springboot/web/demo/pojo/Student.java
@@ -2,8 +2,10 @@ package com.summer.springboot.web.demo.pojo;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class Student {
+public class Student implements Serializable {
     private Long id;
     private String name;
     private Integer age;
