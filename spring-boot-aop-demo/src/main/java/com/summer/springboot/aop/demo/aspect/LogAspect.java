78bc38329d010bd3be9384693fdf76bd71fd26b2
xia45399 <453993887@qq.com>
2019-06-24 09:02:34 +0800

    aop

--- spring-boot-aop-demo/src/main/java/com/summer/springboot/aop/demo/aspect/LogAspect.java
+++ spring-boot-aop-demo/src/main/java/com/summer/springboot/aop/demo/aspect/LogAspect.java
@@ -9,9 +9,6 @@ import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -20,14 +17,19 @@ import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Aspect
 @Component
 @Order(1)
+/**
+ * 先doAround,(joinPoint.proceed之前)
+ * 后doBefore
+ * 切面方法
+ * doAround (异常没有此步)
+ * doAfter
+ * AfterReturning (异常没有此步)
+ * AfterThrowing   （异常执行）
+ */
 public class LogAspect {
 
     @Resource
@@ -107,34 +109,6 @@ public class LogAspect {
         return s.toString();
     }
 
-    private Object getParameter(Method method, ProceedingJoinPoint joinPoint) {
-        Object[] args = joinPoint.getArgs();
-        List<Object> argList = new ArrayList<>();
-        Parameter[] parameters = method.getParameters();
-        for (int i = 0; i < parameters.length; i++) {
-            RequestBody requestBody = parameters[i].getAnnotation(RequestBody.class);
-            if (requestBody != null) {
-                argList.add(args[i]);
-            }
-            RequestParam requestParam = parameters[i].getAnnotation(RequestParam.class);
-            if (requestParam != null) {
-                Map<String, Object> map = new HashMap<>();
-                String key = parameters[i].getName();
-                if (!StringUtils.isEmpty(requestParam.value())) {
-                    key = requestParam.value();
-                }
-                map.put(key, args[i]);
-                argList.add(map);
-            }
-        }
-        if (argList.size() == 0) {
-            return null;
-        } else if (argList.size() == 1) {
-            return argList.get(0);
-        } else {
-            return argList;
-        }
-    }
 
     @AfterReturning(returning = "ret", pointcut = "webLog()")
     public void doAfterReturning(Object ret) throws Throwable {
